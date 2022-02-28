340bd7013aa47c6f7247e933082cf04f8cd3c203
Ilker Ozin <ilker.ozin@selebra.net>
2022-02-28 10:12:38 +0300

    added findCountries

--- api/controllers/countrycontroller.js
+++ api/controllers/countrycontroller.js
@@ -2,11 +2,43 @@ const AWS = require("aws-sdk");
 
 const docClient = new AWS.DynamoDB.DocumentClient();
 const table = "mw-dynamodb-";
-const search =  (req,res) => {
 
-}
 exports.getCountries = (req, res, next) => {
+  var searchName = req.query.searchName;
 
+  if (searchName == undefined) {
+    return fetchCountries(res);
+  } else {
+    return findCountries(searchName, res);
+  }
+};
+
+exports.getCountry = (req, res, next) => {
+  const Alpha2 = req.params.Alpha2;
+
+  var params = {
+    TableName: table,
+    Key: {
+      PK: "resource#country",
+      SK: "Alpha2#" + Alpha2,
+    },
+  };
+
+  var result = docClient.get(params, function (err, data) {
+    if (err) {
+      console.error(
+        "Unable to read item. Error JSON:",
+        JSON.stringify(err, null, 2)
+      );
+    } else {
+      console.log("GetItem succeeded:", JSON.stringify(data.Item, null, 2));
+      res.send(data.Item);
+    }
+  });
+  return result;
+};
+
+function findCountries(searchName, res) {
   var params = {
     TableName: table,
     KeyConditionExpression: "#PK = :PK and begins_with(#SK,:SK)",
@@ -16,10 +48,9 @@ exports.getCountries = (req, res, next) => {
     },
     ExpressionAttributeValues: {
       ":PK": "resource#country",
-      ":SK": "Alpha2#",
+      ":SK": "SearchName#" + searchName,
     },
     Limit: 20,
-    
   };
 
   var result = docClient.query(params, function (err, data) {
@@ -34,29 +65,33 @@ exports.getCountries = (req, res, next) => {
     }
   });
   return result;
-};
+}
 
-exports.getCountry = (req, res, next) => {
-    const Alpha2 = req.params.Alpha2;
-
-    var params = {
-        TableName: table,
-        Key: {
-          PK: "resource#country",
-          SK: "Alpha2#" + Alpha2,
-        },
-      };
-  
-    var result = docClient.get(params, function (err, data) {
-      if (err) {
-        console.error(
-          "Unable to read item. Error JSON:",
-          JSON.stringify(err, null, 2)
-        );
-      } else {
-        console.log("GetItem succeeded:", JSON.stringify(data.Item, null, 2));
-        res.send(data.Item);
-      }
-    });
-    return result;
+function fetchCountries(res) {
+  var params = {
+    TableName: table,
+    KeyConditionExpression: "#PK = :PK and begins_with(#SK,:SK)",
+    ExpressionAttributeNames: {
+      "#PK": "PK",
+      "#SK": "SK",
+    },
+    ExpressionAttributeValues: {
+      ":PK": "resource#country",
+      ":SK": "Alpha2#",
+    },
+    Limit: 20,
   };
+
+  var result = docClient.query(params, function (err, data) {
+    if (err) {
+      console.error(
+        "Unable to read item. Error JSON:",
+        JSON.stringify(err, null, 2)
+      );
+    } else {
+      console.log("GetItem succeeded:", JSON.stringify(data.Items, null, 2));
+      res.send(data.Items);
+    }
+  });
+  return result;
+}
